a8f94cd1ed5d39e282ed1afd2c5fc229810df99d
shachash1984 <shachash1984@gmail.com>
2019-11-10 08:09:14 +0200

    initial commit

---
+++ Assets/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Target : MonoBehaviour
+{
+    private NavMeshAgent[] _navMeshAgents;
+    private Transform _targetTransform;
+
+
+    private void Start()
+    {
+        _navMeshAgents = FindObjectsOfType<NavMeshAgent>();
+        _targetTransform = transform;
+    }
+
+    private void UpdateTarget(Vector3 pos)
+    {
+        transform.position = pos;
+        for (int i = 0; i < _navMeshAgents.Length; i++)
+        {
+            _navMeshAgents[i].SetDestination(pos);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Vector3 newPos = hit.point;
+                UpdateTarget(newPos);
+            }
+        }
+    }
+}
